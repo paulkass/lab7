3f07d996ec86841a9b4c16559f1ca6f7d3fab730
paulkass <pkassian@mail.ccsf.edu>
2014-04-19 12:18:31 -0700

    Finish user signup

---
+++ vendor/config/initializers/backup_secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '5e589cfe38d341660254fa0bdd146768737fab411e3083c7231e7d5e1a26c755a5e8ec7928797360d2c30b136114b321f781ee75fbd1bba9d877072b963df8ae'
